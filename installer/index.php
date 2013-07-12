557255706d9669157627e7b831f2dcddcb4c7a93
Josh Fradley <josh.fradley@gmail.com>
2013-07-12 17:46:56 +0100

    Still working on this upgrade process

--- installer/index.php
+++ installer/index.php
@@ -10,21 +10,21 @@
 <title>Burden &middot; Installer</title>
 <meta name="robots" content="noindex, nofollow">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link href="resources/bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+<link href="../resources/bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet">
 <style type="text/css">
 body {
     padding-top: 60px;
 }
 </style>
-<link href="resources/bootstrap/css/bootstrap-responsive.min.css" type="text/css" rel="stylesheet">
+<link href="../resources/bootstrap/css/bootstrap-responsive.min.css" type="text/css" rel="stylesheet">
 <!-- Javascript start -->
 <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
 <!--[if lt IE 9]>
 <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
-<script src="resources/jquery.min.js"></script>
-<script src="resources/bootstrap/js/bootstrap.min.js"></script>
-<script src="resources/validation/jqBootstrapValidation.js"></script>
+<script src="../resources/jquery.min.js"></script>
+<script src="../resources/bootstrap/js/bootstrap.min.js"></script>
+<script src="../resources/validation/jqBootstrapValidation.js"></script>
 <script type="text/javascript">
 $(document).ready(function() {
     $("input").not("[type=submit]").jqBootstrapValidation();
@@ -49,19 +49,12 @@ $(document).ready(function() {
 </div>
 <?php
 
-//Check if we should update
-if (!isset($_GET["doupdate"])) {
-    if ((file_exists("config.php")) && (!file_exists(".installdone"))) {
-        die("<div class=\"alert alert-info\"><h4 class=\"alert-heading\">Update Required</h4><p>Burden has been updated to the latest version but its config and databases need to be updated too.<p><a href=\"install.php?doupdate\" class=\"btn btn-info\">Run Update</a></p></div></div></body></html>");
-    }
+//Check if Burden has been installed
+if (file_exists("../config.php")) {
+    die("<div class=\"alert alert-info\"><h4 class=\"alert-heading\">Information</h4><p>Burden has already been installed! To reinstall the app please delete your config file and run this installer again.</p><p><a class=\"btn btn-info\" href=\"javascript:history.go(-1)\">Go Back</a></p></div></div></body></html>");
 }
 
-//Check if we are installed
-if (file_exists(".installdone")) {
-    die("<div class=\"alert alert-info\"><h4 class=\"alert-heading\">Information</h4><p>Burden has already been installed! To reinstall the app please delete your .installdone file and run this installer again.</p><p><a class=\"btn btn-info\" href=\"javascript:history.go(-1)\">Go Back</a></p></div></div></body></html>");
-}
-
-if (isset($_GET["doinstall"])) {
+if (isset($_POST["doinstall"])) {
 
     $dbhost = $_POST["dbhost"];
     $dbuser = $_POST["dbuser"];
@@ -79,7 +72,7 @@ if (isset($_GET["doinstall"])) {
     }
     $uniquekey = md5(microtime().rand());
 
-    $installstring = "<?php\n\n//Database Settings\ndefine('DB_HOST', " . var_export($dbhost, true) . ");\ndefine('DB_USER', " . var_export($dbuser, true) . ");\ndefine('DB_PASSWORD', " . var_export($dbpassword, true) . ");\ndefine('DB_NAME', " . var_export($dbname, true) . ");\n\n//Admin Details\ndefine('ADMIN_USER', " . var_export($adminuser, true) . ");\ndefine('ADMIN_PASSWORD', " . var_export($adminpassword, true) . ");\ndefine('SALT', " . var_export($salt, true) . ");\n\n//Other Settings\ndefine('UNIQUE_KEY', " . var_export($uniquekey, true) . ");\ndefine('THEME', 'default');\n\n?>";
+    $installstring = "<?php\n\n//Database Settings\ndefine('DB_HOST', " . var_export($dbhost, true) . ");\ndefine('DB_USER', " . var_export($dbuser, true) . ");\ndefine('DB_PASSWORD', " . var_export($dbpassword, true) . ");\ndefine('DB_NAME', " . var_export($dbname, true) . ");\n\n//Admin Details\ndefine('ADMIN_USER', " . var_export($adminuser, true) . ");\ndefine('ADMIN_PASSWORD', " . var_export($adminpassword, true) . ");\ndefine('SALT', " . var_export($salt, true) . ");\n\n//Other Settings\ndefine('UNIQUE_KEY', " . var_export($uniquekey, true) . ");\ndefine('THEME', 'default');\n\ndefine('BURDEN_VERSION', '1.5');\n\n?>";
 
     //Check if we can connect
     $con = mysql_connect($dbhost, $dbuser, $dbpassword);
@@ -109,86 +102,17 @@ if (isset($_GET["doinstall"])) {
     mysql_query($createtable);
 
     //Write Config
-    $configfile = fopen("config.php", "w");
+    $configfile = fopen("../config.php", "w");
     fwrite($configfile, $installstring);
     fclose($configfile);
-    
-    //Write installdone
-    $installdonefile = fopen(".installdone", "w");
-    fwrite($installdonefile, "Burden successfully installed");
-    fclose($installdonefile);
 
     mysql_close($con);
     
     die("<div class=\"alert alert-success\"><h4 class=\"alert-heading\">Install Complete</h4><p>Burden has been successfully installed. Please delete the \"installer\" folder from your server, as it poses a potential security risk!</p><p>Your login details are shown below, please make a note of them.</p><ul><li>User: $adminuser</li><li>Password: <i>Password you set during install</i></li></ul><p><a href=\"../login.php\" class=\"btn btn-success\">Go To Login</a></p></div></div></body></html>");
-
-} elseif (isset($_GET["doupdate"])) {
-    
-    require_once("config.php");
-
-    $dbhost = DB_HOST;
-    $dbuser = DB_USER;
-    $dbpassword = DB_PASSWORD;
-    $dbname = DB_NAME;
-    $adminuser = ADMIN_USER;
-    //$adminpassword = ADMIN_PASSWORD;
-    //$salt = SALT;
-    $uniquekey = UNIQUE_KEY;
-    $theme = THEME;
-
-    //Salt and hash passwords
-    //From 1.4 --> 1.5
-    $temppassword = substr(str_shuffle("abcdefghijklmnopqrstuvwxyz123456789"), 0, 6);
-    $randsalt = md5(uniqid(rand(), true));
-    $salt = substr($randsalt, 0, 3);
-    $hashedpassword = hash("sha256", $temppassword);
-    $adminpassword = hash("sha256", $salt . $hashedpassword);
-    
-    $updatestring = "<?php\n\n//Database Settings\ndefine('DB_HOST', " . var_export($dbhost, true) . ");\ndefine('DB_USER', " . var_export($dbuser, true) . ");\ndefine('DB_PASSWORD', " . var_export($dbpassword, true) . ");\ndefine('DB_NAME', " . var_export($dbname, true) . ");\n\n//Admin Details\ndefine('ADMIN_USER', " . var_export($adminuser, true) . ");\ndefine('ADMIN_PASSWORD', " . var_export($adminpassword, true) . ");\ndefine('SALT', " . var_export($salt, true) . ");\n\n//Other Settings\ndefine('UNIQUE_KEY', " . var_export($uniquekey, true) . ");\ndefine('THEME', 'default');\n\n?>";
-    
-    //Check if we can connect
-    $con = mysql_connect($dbhost, $dbuser, $dbpassword);
-    if (!$con) {
-        die("<div class=\"alert alert-error\"><h4 class=\"alert-heading\">Update Failed</h4><p>Error: Could not connect to database (" . mysql_error() . "). Check your database settings are correct.</p><p><a class=\"btn btn-danger\" href=\"javascript:history.go(-1)\">Go Back</a></p></div></div></body></html>");
-    }
-
-    //Check if database exists
-    $does_db_exist = mysql_select_db($dbname, $con);
-    if (!$does_db_exist) {
-        die("<div class=\"alert alert-error\"><h4 class=\"alert-heading\">Update Failed</h4><p>Error: Database does not exist (" . mysql_error() . "). Check your database settings are correct.</p><p><a class=\"btn btn-danger\" href=\"javascript:history.go(-1)\">Go Back</a></p></div></div></body></html>");
-    }
-
-    //Alter Data table
-    //From 1.4 --> 1.5
-    $altertable = "ALTER TABLE `Data`
-    CHANGE `id` `id` SMALLINT(10) NOT NULL,
-    CHANGE `category` `category` VARCHAR(20) NOT NULL,
-    CHANGE `priority` `highpriority` TINYINT(1) NOT NULL,
-    CHANGE `task` `task` VARCHAR(300) NOT NULL,
-    CHANGE `due` `due` VARCHAR(10) NOT NULL,
-    CHANGE `completed` `completed` TINYINT(1) NOT NULL default \"0\",
-    CHANGE `datecompleted` `datecompleted` VARCHAR(12) NOT NULL";
-
-    //Run query
-    mysql_query($altertable);
-
-    //Write Config
-    $configfile = fopen("config.php", "w");
-    fwrite($configfile, $updatestring);
-    fclose($configfile);
-    
-    //Write installdone
-    $installdonefile = fopen(".installdone", "w");
-    fwrite($installdonefile, "Burden successfully installed");
-    fclose($installdonefile);
-    
-    mysql_close($con);
-    
-    die("<div class=\"alert alert-success\"><h4 class=\"alert-heading\">Update Complete</h4><p>Burden has been successfully updated. Because Burden 1.5 uses salt password hashing, your password is now <b>$temppassword.</b> Please change it to something more memorable as soon as possible.<p><a href=\"login.php\" class=\"btn btn-success\">Go To Login</a></p></div></div></body></html>");
 }
 
 ?>	
-<form action="?doinstall" method="post">
+<form method="post">
 <fieldset>
 <h4>Database Settings</h4>
 <div class="control-group">
---
+++ installer/upgrade.php
@@ -0,0 +1,147 @@
+<?php
+
+//Burden, Copyright Josh Fradley (http://github.com/joshf/Burden)
+
+if (!file_exists("../config.php")) {
+    header("Location: index.php");
+}
+
+require_once("../config.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Burden &middot; Installer</title>
+<meta name="robots" content="noindex, nofollow">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="../resources/bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+<style type="text/css">
+body {
+    padding-top: 60px;
+}
+</style>
+<link href="../resources/bootstrap/css/bootstrap-responsive.min.css" type="text/css" rel="stylesheet">
+<!-- Javascript start -->
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<script src="../resources/jquery.min.js"></script>
+<script src="../resources/bootstrap/js/bootstrap.min.js"></script>
+<script src="../resources/validation/jqBootstrapValidation.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+    $("input").not("[type=submit]").jqBootstrapValidation();
+});
+</script>
+<!-- Javascript end -->
+</head>
+<body>
+<!-- Nav start -->
+<div class="navbar navbar-fixed-top">
+<div class="navbar-inner">
+<div class="container">
+<a class="brand" href="#">Burden</a>
+</div>
+</div>
+</div>
+<!-- Nav end -->
+<!-- Content start -->
+<div class="container">
+<div class="page-header">
+<h1>Upgrade</h1>
+</div>
+<?php
+
+$version = "1.5";
+
+if ($version == BURDEN_VERSION) {
+    die("<div class=\"alert alert-info\"><h4 class=\"alert-heading\">Upgrade Notice</h4><p>Burden does not require an upgrade<p><a href=\"../login.php\" class=\"btn btn-info\">Go To Login</a></p></div></div></body></html>");
+    
+}
+
+$dbhost = DB_HOST;
+$dbuser = DB_USER;
+$dbpassword = DB_PASSWORD;
+$dbname = DB_NAME;
+$adminuser = ADMIN_USER;
+//$adminpassword = ADMIN_PASSWORD;
+//$salt = SALT;
+$uniquekey = UNIQUE_KEY;
+$theme = THEME;
+
+//Salt and hash passwords
+//From 1.4 --> 1.5
+$temppassword = substr(str_shuffle("abcdefghijklmnopqrstuvwxyz123456789"), 0, 6);
+$randsalt = md5(uniqid(rand(), true));
+$salt = substr($randsalt, 0, 3);
+$hashedpassword = hash("sha256", $temppassword);
+$adminpassword = hash("sha256", $salt . $hashedpassword);
+
+$updatestring = "<?php
+
+//Database Settings
+define('DB_HOST', " . var_export($dbhost, true) . ");
+define('DB_USER', " . var_export($dbuser, true) . ");
+define('DB_PASSWORD', " . var_export($dbpassword, true) . ");
+define('DB_NAME', " . var_export($dbname, true) . ");
+
+//Admin Details
+define('ADMIN_USER', " . var_export($adminuser, true) . ");
+define('ADMIN_PASSWORD', " . var_export($adminpassword, true) . ");
+define('SALT', " . var_export($salt, true) . ");
+
+//Other Settings
+define('UNIQUE_KEY', " . var_export($uniquekey, true) . ");
+define('THEME', 'default');
+define('BURDEN_VERSION', " . var_export($version, true) . ");
+
+?>";
+
+//Check if we can connect
+$con = mysql_connect($dbhost, $dbuser, $dbpassword);
+if (!$con) {
+    die("<div class=\"alert alert-error\"><h4 class=\"alert-heading\">Update Failed</h4><p>Error: Could not connect to database (" . mysql_error() . "). Check your database settings are correct.</p><p><a class=\"btn btn-danger\" href=\"javascript:history.go(-1)\">Go Back</a></p></div></div></body></html>");
+}
+
+//Check if database exists
+$does_db_exist = mysql_select_db($dbname, $con);
+if (!$does_db_exist) {
+    die("<div class=\"alert alert-error\"><h4 class=\"alert-heading\">Update Failed</h4><p>Error: Database does not exist (" . mysql_error() . "). Check your database settings are correct.</p><p><a class=\"btn btn-danger\" href=\"javascript:history.go(-1)\">Go Back</a></p></div></div></body></html>");
+}
+
+//Alter Data table
+//From 1.4 --> 1.5
+$altertable = "ALTER TABLE `Data`
+CHANGE `id` `id` SMALLINT(10) NOT NULL,
+CHANGE `category` `category` VARCHAR(20) NOT NULL,
+CHANGE `priority` `highpriority` TINYINT(1) NOT NULL,
+CHANGE `task` `task` VARCHAR(300) NOT NULL,
+CHANGE `due` `due` VARCHAR(10) NOT NULL,
+CHANGE `completed` `completed` TINYINT(1) NOT NULL default \"0\",
+CHANGE `datecompleted` `datecompleted` VARCHAR(12) NOT NULL";
+
+//Run query
+mysql_query($altertable);
+
+//Make sure user doesnt lose their priorities
+mysql_query("UPDATE Data SET highpriority = \"1\" WHERE highpriority >= \"4\"");
+
+//Write Config
+$configfile = fopen("../config.php", "w");
+fwrite($configfile, $updatestring);
+fclose($configfile);
+
+mysql_close($con);
+
+?>
+<div class="alert alert-success">
+<h4 class="alert-heading">Update Complete</h4>
+<p>Burden has been successfully updated. Because Burden 1.5 uses salt password hashing, your password is now <b><?php echo $temppassword; ?>.</b> Please change it to something more memorable as soon as possible.<p><a href="../login.php" class="btn btn-success">Go To Login</a></p>
+</div>
+</div>
+<!-- Content end -->
+</body>
+</html>
