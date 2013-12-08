14585dfffaea9bf6da2d87196aedd7b43515e607
Josh Fradley <josh.fradley@gmail.com>
2013-12-08 12:20:09 +0000

    Release 1.8

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 //Burden, Copyright Josh Fradley (http://github.com/joshf/Burden)
 
-$version = "1.7";
+$version = "1.8";
 
 if (!file_exists("config.php")) {
     die("Error: Config file not found! Please reinstall Burden.");
--- installer/upgrade.php
+++ installer/upgrade.php
@@ -21,7 +21,7 @@ if (!$does_db_exist) {
 }
 
 //Define Version
-$version = "1.7";
+$version = "1.8";
 
 if ($version == VERSION) {
     die("Information: The latest version of Burden is already installed and an upgrade is not required.");
