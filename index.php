c24e1a13fed48899f327df08e2667f26ecbb5482
Josh Fradley <josh.fradley@gmail.com>
2013-12-22 12:53:12 +0000

    Leave that one alone

--- index.php
+++ index.php
@@ -166,7 +166,7 @@ echo "<noscript><div class=\"alert alert-info\"><h4 class=\"alert-heading\">Info
 
 //Update checking
 if (!isset($_COOKIE["burdenhascheckedforupdates"])) {
-    $remoteversion = file_get_contents("https://raw.github.com/joshf/Burden/master/assets/version.php");
+    $remoteversion = file_get_contents("https://raw.github.com/joshf/Burden/master/version.txt");
     if (preg_match("/^[0-9.-]{1,}$/", $remoteversion)) {
         if ($version < $remoteversion) {
             echo "<div class=\"alert alert-warning\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">&times;</button><h4 class=\"alert-heading\">Update</h4><p>Burden <a href=\"https://github.com/joshf/Burden/releases/$remoteversion\" class=\"alert-link\" target=\"_blank\">$remoteversion</a> is available. <a href=\"https://github.com/joshf/Burden#updating\" class=\"alert-link\" target=\"_blank\">Click here for instructions on how to update</a>.</p></div>";
