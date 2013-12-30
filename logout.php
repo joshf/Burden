2fcc3cb9b6cb706ddcbac706cb328c73cfcd9777
Josh Fradley <josh.fradley@gmail.com>
2013-12-30 12:58:19 +0000

    Remove unneeded require

--- logout.php
+++ logout.php
@@ -2,12 +2,6 @@
 
 //Burden, Copyright Josh Fradley (http://github.com/joshf/Burden)
 
-if (!file_exists("config.php")) {
-    header("Location: installer");
-}
-
-require_once("config.php");
-
 session_start();
 
 unset($_SESSION["burden_user"]);
