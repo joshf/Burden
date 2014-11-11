e0c8fa7ba0749f38a32dae5dd6206f7e385c43e4
Josh Fradley <josh.fradley@gmail.com>
2014-11-11 22:39:04 +0100

    Remove useless css

--- reset.php
+++ reset.php
@@ -114,10 +114,7 @@ body {
     -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
     box-shadow: 0 1px 2px rgba(0,0,0,.05);
 }
-.form-signin .form-signin-heading {
-    margin-bottom: 10px;
-}
-.form-signin input[type="text"], .form-signin input[type="password"] {
+.form-signin input[type="email"] {
     font-size: 16px;
     height: auto;
     margin-bottom: 5px;
